e0835ac3178315ff524a1a6b7ae4eaad5ae36769
27-AB <birhanetamirat0@gmail.com>
2026-06-16 00:26:15 -0700

    Fix typo in leap year function comment

--- DAY-5/functins.py
+++ DAY-5/functins.py
@@ -1,4 +1,4 @@
-# problem given year determie wether is a leap year. If it is a lea year, return the Boolean true, otherwise return False.
+# problem given year determine wether is a leap year. If it is a leap year, return the Boolean true, otherwise return False.
 
 def is_leap(year):
     leap = False
